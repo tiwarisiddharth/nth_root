7b1a979f9539055805f63707124308703ad30636
Siddharth Tiwari <siddy1101@gmail.com>
2018-06-12 02:48:26 +0530

    Update nth_root.py

--- nth_root.py
+++ nth_root.py
@@ -6,9 +6,16 @@ def nthRootMe(m,n):
         for i in range(0,q):
             r*=p
         return r
+    #this repe function takes a guess and improves it and keeps improving it until the if condition is met.
     def repe(x):
         a=m/pwr(x,n-1)
         b=(a+(x*(n-1)))/n
+        
+        # this if specifies when the new guess generated will have no change then the that will be the answer.
+        # to adjust precision
+        # use this 'abs(((x+b)/2)-x) < p' as a condition inside if and replace 'p' with the amount of precision you need
+        # smaller the value of 'p' larger the precision.
+        
         if (x==((x+b)/2)):
             print(x)
         else:
